8497c93d9564ab0d824c3d5e0061ed1015186da0
manhkun95 <nguyenduymanh0406@gmail.com>
2018-09-26 17:51:15 +0700

    first commit

---
+++ contracts/Adoption.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.17;
+
+contract Adoption {
+    address[16] public adopters;
+    function adopt(uint petId) public returns (uint) {
+        require(petId >= 0 && petId <= 15, "pet doesn't exists");
+        adopters[petId] = msg.sender;
+        return petId;
+    }
+    function getAdopter() public view returns (address[16]){
+        return adopters;
+    }
+}
